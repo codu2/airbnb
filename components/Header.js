bcd1850ee81977aef2ea6baaa80d08625495c144
codu2 <khub0929@gmail.com>
2022-10-14 06:59:26 +0900

    react-date-range를 이용해 검색어 입력 시 기간과 인원수를 선택할 수 있는 DateRangePicker 렌더링, next/router의 useRouter를 이용한 페이지 이동 추가

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import {
   MagnifyingGlassIcon,
   Bars3Icon,
@@ -7,11 +7,51 @@ import {
   UsersIcon,
 } from "@heroicons/react/24/solid";
 import { GlobeAltIcon } from "@heroicons/react/24/outline";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { DateRangePicker } from "react-date-range";
+import { useRouter } from "next/router";
 
-function Header() {
+function Header({ placeholder }) {
+  const [searchInput, setSearchInput] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [numberOfGuests, setNumberOfGuests] = useState(1);
+  const router = useRouter();
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
+  const resetInput = () => {
+    setSearchInput("");
+  };
+
+  const search = () => {
+    router.push({
+      pathname: "/search",
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(), // url에서 사용하기 위해 변환
+        endDate: endDate.toISOString(),
+        numberOfGuests: numberOfGuests,
+      },
+    });
+  };
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-sm p-4 md:px-10">
-      <div className="relative flex items-center h-8 cursor-pointer my-auto">
+      <div
+        onClick={() => router.push("/")}
+        className="relative flex items-center h-8 cursor-pointer my-auto"
+      >
         <Image
           src="https://links.papareact.com/qd3"
           alt=""
@@ -22,9 +62,11 @@ function Header() {
       </div>
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
         <input
+          value={searchInput}
+          onChange={(event) => setSearchInput(event.target.value)}
           className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
           type="text"
-          placeholder="어디로 여행가세요?"
+          placeholder={placeholder || "어디로 여행가세요?"}
         />
         <MagnifyingGlassIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
       </div>
@@ -38,6 +80,39 @@ function Header() {
           <UserCircleIcon className="h-6 text-gray-500" />
         </div>
       </div>
+
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()} // 과거를 선택할 수 없도록
+            rangeColors={["#fd5861"]}
+            onChange={handleSelect}
+          />
+          <div className="flex items-center border-b mb-4 pb-2">
+            <h2 className="text-md flex-grow font-semibold">게스트 추가</h2>
+            <UsersIcon className="h-5" />
+            <input
+              value={numberOfGuests}
+              onChange={(event) => setNumberOfGuests(event.target.value)}
+              type="number"
+              min={1}
+              className="w-12 pl-2 text-md outline-none text-red-400"
+            />
+          </div>
+          <div className="flex">
+            <button
+              className="text-sm flex-grow text-gray-500"
+              onClick={resetInput}
+            >
+              Cancel
+            </button>
+            <button onClick={search} className="text-sm flex-grow text-red-400">
+              Search
+            </button>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
